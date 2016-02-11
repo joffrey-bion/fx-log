d3d5baf3ad2d1cb9c875ca51e890d1f87c7c6735
Louis Pontoise <louis.pontoise@amadeus.com>
2016-02-12 00:36:24 +0100

    Commented and cleaned-up a bit MockLogger.java

--- src/test/java/MockLogger.java
+++ src/test/java/MockLogger.java
@@ -1,3 +1,4 @@
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,11 +9,13 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.SYNC;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 /**
- * This class is used to generate sample data.
+ * Tool to simulate a running Weblogic server. It streams mock log lines to a file on the disk.
  */
+@ParametersAreNonnullByDefault
 public class MockLogger {
 
     private static final String[] levels = {"VERBOSE", "DEBUG", "INFO", "WARN", "ERROR"};
@@ -30,26 +33,26 @@ public class MockLogger {
     private static final Random random = new Random();
 
     public static void main(String[] args) {
+        // TODO: Use a command line lib? See http://commons.apache.org/proper/commons-cli/index.html
         streamMockLogsToFile("mock.log", 0);
     }
 
+    /**
+     * Returns a mock log line. Each part of the line is randomly generated.
+     *
+     * @return the mock log line
+     */
     private static String mockLogLine() {
         String format = "####<%s> <%s> <%s> <> <%s;jsessionid=%s>%n";
         return String.format(format, LocalDateTime.now(), rand(levels), rand(classes), randMsg(), randSID());
     }
 
-    private static String rand(String[] array) {
-        return array[random.nextInt(array.length)];
-    }
-
-    private static String randMsg() {
-        return String.format("The %s %s %s", rand(nouns), rand(verbs), rand(participates));
-    }
-
-    private static String randSID() {
-        return random.ints(16, 0, 0xFFFF).mapToObj(Integer::toHexString).collect(Collectors.joining());
-    }
-
+    /**
+     * Writes mock lines to the file at the given path. It writes line by line. Speed depends on delayBetweenLines. The file is cleaned at the beginning of each call.
+     *
+     * @param path              the path to the target file
+     * @param delayBetweenLines the delay between line writes, in milliseconds
+     */
     private static void streamMockLogsToFile(String path, int delayBetweenLines) {
         Path p = Paths.get(path);
         try (BufferedWriter writer = Files.newBufferedWriter(p, UTF_8, TRUNCATE_EXISTING, SYNC)) {
@@ -64,5 +67,17 @@ public class MockLogger {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String rand(String[] array) {
+        return array[random.nextInt(array.length)];
+    }
+
+    private static String randMsg() {
+        return String.format("The %s %s %s", rand(nouns), rand(verbs), rand(participates));
+    }
+
+    private static String randSID() {
+        return random.ints(16, 0, 0xFFFF).mapToObj(Integer::toHexString).collect(Collectors.joining());
     }
 }
