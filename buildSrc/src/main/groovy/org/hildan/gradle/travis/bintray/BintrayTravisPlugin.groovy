394a90f8946ecfc97e3bcd02b7fd742815123518
Joffrey BION <joffrey.bion@gmail.com>
2016-11-15 01:57:37 +0100

    Make travis bintray plugin apply the bintray plugin itself

--- buildSrc/src/main/groovy/org/hildan/gradle/travis/bintray/BintrayTravisPlugin.groovy
+++ buildSrc/src/main/groovy/org/hildan/gradle/travis/bintray/BintrayTravisPlugin.groovy
@@ -1,5 +1,7 @@
 package org.hildan.gradle.travis.bintray
 
+import com.jfrog.bintray.gradle.BintrayPlugin
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,7 +9,9 @@ class BintrayTravisPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-//        project.pluginManager.apply('com.jfrog.bintray')
+        project.pluginManager.apply(BintrayPlugin)
+        BintrayExtension ext = project.extensions.getByType(BintrayExtension)
+
         project.extensions.create(BintrayTravisExtension.NAME, BintrayTravisExtension, project)
         project.task(GenerateBintrayDescriptorTask.NAME, type: GenerateBintrayDescriptorTask)
     }
